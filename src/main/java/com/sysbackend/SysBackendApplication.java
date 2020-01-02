c45957d943f50d898e6d55805a3e1a33eb9e172f
Valter Fernandes <fpvalter@gmail.com>
2020-01-02 10:31:49 -0300

    first commit

---
+++ src/main/java/com/sysbackend/SysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sysbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysbackend/SysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
